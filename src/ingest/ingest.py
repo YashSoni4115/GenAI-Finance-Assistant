4666c460ea7cd6ae74e506bfc8c4ba357e80f4ab
YashSoni4115 <yashsonig@gmail.com>
2026-01-18 00:03:50 -0500

    Implemented ingestion to convert from .csv to .txt file that can be read by an LLM and referenced

---
+++ src/ingest/ingest.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+def load_data(file_path: str) -> str:
+    data = pd.read_csv(file_path)
+    return save_data_to_txt(data, file_path)
+
+def pd_df_to_txt(data: pd.DataFrame, output_path: str, filename: str) -> None:
+    with open(output_path, 'w') as f:
+        for index, row in data.iterrows():
+            f.write(f"Record from: {filename}.csv, line: {index + 1}\n")
+            for col in data.columns:
+                f.write(f"{col}: {row[col]}\n")
+            f.write("\n")
+
+def save_data_to_txt(data: pd.DataFrame, path: str) -> str:
+    filename = os.path.splitext(os.path.basename(path))[0]
+    output_path = '../../corpus/' + filename + '.txt'
+    pd_df_to_txt(data, output_path, filename)
+    return output_path
+
+if __name__ == "__main__":
+    path = '../../data/cibc_finance_terms.csv'
+    data = load_data(path)
